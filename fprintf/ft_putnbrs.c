7f04f4a86ec9839fc82f634be9840e451de2e8bc
eweiberl <eweiberl@student.42heilbronn.de>
2023-10-31 16:55:00 +0100

    fixed function names

--- fprintf/ft_putnbrs.c
+++ fprintf/ft_putnbrs.c
@@ -6,7 +6,7 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 18:43:32 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/10/31 16:53:31 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/10/31 16:54:45 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_fputint(int fd, int n)
 	int	ccount;
 
 	ccount = 0;
-	if (ft_putnbr(fd, n) < 0)
+	if (ft_fputnbr(fd, n) < 0)
 		return (-1);
 	if (n <= 0)
 		ccount++;
@@ -38,11 +38,11 @@ int	ft_fputnbr(int fd, int n)
 	count = 0;
 	usi = n;
 	if (n == 0)
-		return (ft_putchar(fd, '0'));
+		return (ft_fputchar(fd, '0'));
 	if (n < 0)
 	{
 		usi = -n;
-		if (ft_putchar(fd, '-') < 0)
+		if (ft_fputchar(fd, '-') < 0)
 			return (-1);
 	}
 	while (usi > 0)
@@ -52,7 +52,7 @@ int	ft_fputnbr(int fd, int n)
 		count++;
 	}
 	while (count > 0)
-		if (ft_putchar(fd, cnt[--count]) < 0)
+		if (ft_fputchar(fd, cnt[--count]) < 0)
 			return (-1);
 	return (0);
 }
@@ -64,7 +64,7 @@ int	ft_fputhex(int fd, long n, char *hex)
 
 	i = 0;
 	if (n == 0)
-		return (ft_putchar(fd, '0'));
+		return (ft_fputchar(fd, '0'));
 	while (n != 0)
 	{
 		buff[i] = hex[n % 16];
@@ -74,7 +74,7 @@ int	ft_fputhex(int fd, long n, char *hex)
 	n = i;
 	while (i > 0)
 	{
-		if (ft_putchar(fd, buff[--i]) < 0)
+		if (ft_fputchar(fd, buff[--i]) < 0)
 			return (-1);
 	}
 	return (n);
@@ -90,7 +90,7 @@ int	ft_fput_ptr(int fd, void *ptr)
 	hex = "0123456789abcdef";
 	i = 0;
 	if (ptr == NULL)
-		return (ft_putstr(fd, "0x0"));
+		return (ft_fputstr(fd, "0x0"));
 	n = (long long)ptr;
 	while (n != 0)
 	{
@@ -98,12 +98,12 @@ int	ft_fput_ptr(int fd, void *ptr)
 		n = n / 16;
 		i++;
 	}
-	if (ft_putstr(fd, "0x") < 0)
+	if (ft_fputstr(fd, "0x") < 0)
 		return (-1);
 	n = i + 2;
 	while (i > 0)
 	{
-		if (ft_putchar(fd, buff[--i]) < 0)
+		if (ft_fputchar(fd, buff[--i]) < 0)
 			return (-1);
 	}
 	return (n);
@@ -116,7 +116,7 @@ int	ft_fputunsigned(int fd, unsigned int n)
 
 	count = 0;
 	if (n == 0)
-		return (ft_putchar(fd, '0'));
+		return (ft_fputchar(fd, '0'));
 	while (n > 0)
 	{
 		cnt[count] = (n % 10) + 48;
@@ -127,7 +127,7 @@ int	ft_fputunsigned(int fd, unsigned int n)
 	while (count > 0)
 	{
 		count--;
-		if (ft_putchar(fd, cnt[count]) < 0)
+		if (ft_fputchar(fd, cnt[count]) < 0)
 			return (-1);
 	}
 	return (n);
