b43d37b4d6562a2d833ff8cb1571c3d5d3bdcdb7
eweiberl <eweiberl@student.42heilbronn.de>
2023-10-31 16:52:20 +0100

    updated header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 09:36:37 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/05/02 17:33:09 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/10/31 16:42:49 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@
 # include "write/write.h"
 # include "printf/ft_printf.h"
 # include "gnl/get_next_line.h"
+# include "fprintf/ft_fprintf.h"
 
 #endif /* LIBFT_H */
